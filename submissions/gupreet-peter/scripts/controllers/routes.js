00c5eb06be33f95cb329270f1949069afb65aff0
Peter Breen <pbreen@gmail.com>
2016-05-23 14:07:49 -0700

    fixed routes.js function call

--- submissions/gupreet-peter/scripts/controllers/routes.js
+++ submissions/gupreet-peter/scripts/controllers/routes.js
@@ -1,8 +1,8 @@
 /* DONE: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
-   page('/', articlesController);
-   page('/about', aboutController);
+   page('/', articlesController.index);
+   page('/about', aboutController.index);
 
 
 /* DONE: What function do we call to activate page.js?
